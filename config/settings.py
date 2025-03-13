e85a251c6026536acf241411543ce935f9a2846b
Dmitry_Bezgubov <dmitrij-bezgubov@yandex.ru>
2025-03-13 19:11:58 +0500

    Create bootstrap

--- config/settings.py
+++ config/settings.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 
-from django.conf.global_settings import AUTH_USER_MODEL
+from django.conf.global_settings import AUTH_USER_MODEL, MEDIA_ROOT, MEDIA_URL
 from dotenv import load_dotenv
 
 load_dotenv(override=True)
@@ -120,6 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),  # Указываем путь к общей папке static
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # Для сбора статики
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
