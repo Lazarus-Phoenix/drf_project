60bab5f2d2b9358e239800cdff60fa7f3858c87e
Dmitry_Bezgubov <dmitrij-bezgubov@yandex.ru>
2025-03-20 11:50:17 +0500

    Добавление доп полей сериализатора курсов

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,15 +1,31 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from lms.models import Course, Lesson
 
 
 class CourseSerializer(ModelSerializer):
+    lessons_count = serializers.SerializerMthod()
+    lessons_info = serializers.SerializerMethodField()
+
+    def get_lessons_count(self, obj):
+        # Подсчет количества уроков, связанных с данным курсом
+
+        return obj.lesson.count()
+
+    def get_lessons_info(self, obj):
+        # Получение информации об уроках, связанных с данным курсом
+
+        return LessonSerializer(obj.lessons.all(), many=True).data
+
     class Meta:
         model = Course
-        fields = "__all__"
+        fields = ['id', 'name', 'description', 'lessons_count', 'lessons_info']
 
 
 class LessonSerializer(ModelSerializer):
+
+
     class Meta:
         model = Lesson
         fields = "__all__"
