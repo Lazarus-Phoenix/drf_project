e173863b6f21620cf52b946a04ea7d39b87a8c6e
dima <dmitrij-bezgubov@yandex.ru>
2025-04-12 13:54:15 +0500

    Подготовка окружения к задачам асинхорнного исполнения

--- config/settings.py
+++ config/settings.py
@@ -136,3 +136,21 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=300),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
+
+
+# Настройки для Celery
+
+# # URL-адрес брокера сообщений
+# CELERY_BROKER_URL = 'redis://localhost:6379' # Например, Redis, который по умолчанию работает на порту 6379
+#
+# # URL-адрес брокера результатов, также Redis
+# CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+#
+# # Часовой пояс для работы Celery
+# CELERY_TIMEZONE = "Australia/Tasmania"
+#
+# # Флаг отслеживания выполнения задач
+# CELERY_TASK_TRACK_STARTED = True
+#
+# # Максимальное время на выполнение задачи
+# CELERY_TASK_TIME_LIMIT = 30 * 60
