f3ef1b837379e2cbccab057115daf3cce3c8517f
dima <dmitrij-bezgubov@yandex.ru>
2025-05-10 04:12:20 +0500

    Writing README.md

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ STRIPE_API_KEY = os.getenv("STRIPE_API_KEY")
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -67,9 +67,9 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.getenv('NAME'),
-        "USER": os.getenv('USER'),
-        "PASSWORD": os.getenv('PASSWORD'),
+        "NAME": os.getenv("NAME"),
+        "USER": os.getenv("USER"),
+        "PASSWORD": os.getenv("PASSWORD"),
         "HOST": os.getenv("HOST"),
         "PORT": os.getenv("PORT"),
     }
@@ -108,7 +108,7 @@ USE_TZ = True
 
 
 STATIC_URL = "static/"
-STATIC_ROOT = '/drf_project/staticfiles/'
+STATIC_ROOT = "/drf_project/staticfiles/"
 STATICFILES_DIRS = (BASE_DIR / "static",)
 
 MEDIA_URL = "media/"
@@ -158,11 +158,11 @@ SIMPLE_JWT = {
 
 # URL-адрес брокера сообщений
 CELERY_BROKER_URL = os.getenv(
-    "CELERY_BROKER_URL", 'redis://redis:6379/0'
+    "CELERY_BROKER_URL", "redis://redis:6379/0"
 )  # Например, Redis, который по умолчанию работает на порту 6379
 
 # URL-адрес брокера результатов, также Redis
-CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND", 'redis://redis:6379/0')
+CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND", "redis://redis:6379/0")
 
 # Часовой пояс для работы Celery
 CELERY_TIMEZONE = TIME_ZONE
