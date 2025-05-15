7bd191023e8d60f1689592b058cee5b56f1482be
dima <dmitrij-bezgubov@yandex.ru>
2025-05-16 00:14:24 +0500

    Закат deploy вручную попытка №2

--- config/settings.py
+++ config/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",  # required for serving swagger ui's css/js files
     "drf_yasg",
     "django_celery_beat",
+    "django_celery_results",
     "celery",
 ]
 
