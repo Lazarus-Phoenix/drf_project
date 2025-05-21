6dc55eb26ae0a1396dae565e03659fa842020439
dima <dmitrij-bezgubov@yandex.ru>
2025-05-22 00:21:46 +0500

    Исправление путей nginx

--- config/settings.py
+++ config/settings.py
@@ -119,14 +119,19 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR /'static']
+STATICFILES_DIRS = [
+    os.path.join(
+        BASE_DIR,
+        'static'
+    )
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # STATIC_ROOT = '/drf_project/staticfiles/'
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
